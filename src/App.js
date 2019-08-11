e56d42c965849e4ded98584323e9445b9cfc9d12
Denis John <denis.john.ndarake@gmail.com>
2019-08-12 00:30:46 +0100

    Cleaned up the code and completed the core todo list app with static state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Meeting 1 with them',
+        completed: false,
+      },
+      {
+        id: 2,
+        title: 'Meeting 1 with them',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Meeting 1 with them',
+        completed: true,
+      }
+    ]
+  };
+
+  markComplete = (id) =>{
+      this.setState({ todos: this.state.todos.map(todo => {
+        if(todo.id === id){
+          todo.completed = !todo.completed
+        }
+        return todo;
+      }) })
+  }
+
+  render(){
+    console.log(this.state.todos);
+    return(
+      <div className="App">
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
+      </div>
+    );
+  }
 }
 
 export default App;
