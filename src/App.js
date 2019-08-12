80baf73279199433b45357a367e0813f5f3ed612
Denis John <denis.john.ndarake@gmail.com>
2019-08-12 13:57:54 +0100

    Added route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Todos from "./components/Todos";
 import Header from "./components/layout/Header";
 import AddTodo from "./components/AddTodo";
+import About from "./components/pages/About";
 import uuid from "uuid";
 
 class App extends React.Component {
@@ -55,17 +57,28 @@ class App extends React.Component {
   render() {
     console.log(this.state.todos);
     return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <AddTodo addTodo={this.addTodo} />
-          <Todos
-            todos={this.state.todos}
-            markComplete={this.markComplete}
-            delTodo={this.delTodo}
-          />
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,20 +1,32 @@
-import React from 'react';
-import { defaultCipherList } from 'constants';
+import React from "react";
+import { Link } from "react-router-dom";
+import { link } from "fs";
 
-function Header(){
-    return (
-        <header style={headerStyle}>
-            <h1>TODO LIST</h1>
-        </header>
-    )
+function Header() {
+  return (
+    <header style={headerStyle}>
+      <h1>TODO LIST</h1>
+      <Link to="/" style={linkStyle}>
+        Home
+      </Link>{" "}
+      |{" "}
+      <Link to="/about" style={linkStyle}>
+        About
+      </Link>
+    </header>
+  );
 }
 
+const linkStyle = {
+  color: "#fff",
+  textDecoration: "none"
+};
+
 const headerStyle = {
-    background: '#333',
-    color: '#fff',
-    textAlign: 'center',
-    padding: '10px',
-  }
-  
+  background: "#333",
+  color: "#fff",
+  textAlign: "center",
+  padding: "10px"
+};
 
 export default Header;
